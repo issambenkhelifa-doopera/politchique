38ed78b18ba285d1937b84dd404f26927881d38c
Maxime Rat <38426365+maximerat@users.noreply.github.com>
2026-05-04 16:57:14 +0200

    f

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,9 @@ export default defineNuxtConfig({
   uiPro: {
     license: "18664695-FBE3-4265-B599-794BC7843E8D",
   },
+  colorMode: {
+    preference: 'light',
+  },
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
 })
